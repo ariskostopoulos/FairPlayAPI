b7bce0bcbf7662e38eb867e54521d0062a7a3521
Aris Kostopoulos <arisbkostopoulos@gmail.com>
2021-09-11 19:54:21 +0300

    initial commit

---
+++ pilot-fairplay/src/main/java/com/fairplay/pilotfairplay/PilotFairplayApplication.java
@@ -0,0 +1,13 @@
+package com.fairplay.pilotfairplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotFairplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotFairplayApplication.class, args);
+	}
+
+}
---
+++ pilot-fairplay/src/test/java/com/fairplay/pilotfairplay/PilotFairplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fairplay.pilotfairplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotFairplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d2ac62d6d1e04bd213ab689b03dc36c2a8d4f267
stavros121 <stavros121@protonmail.com>
2021-09-12 13:24:37 +0300

    Datasource config

--- pilot-fairplay/web/src/main/java/com/fairplay/web/config/SpringBootConfig.java
+++ pilot-fairplay/web/src/main/java/com/fairplay/web/config/SpringBootConfig.java
@@ -1,7 +1,9 @@
 package com.fairplay.web.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * Spring Boot configuration class.
@@ -11,4 +13,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("com.fairplay")
 public class SpringBootConfig {
+
 }
